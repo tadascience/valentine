1af10c4bd9afd790400de2ca34d099feb145ec62
Romain Francois <romain@tada.science>
2025-02-09 11:01:13 +0100

    some finer error handling

--- R/roses.R
+++ R/roses.R
@@ -6,12 +6,12 @@
 #' @param pkg A package
 #' @param hint extra information to add to the prompt
 #' @param emoji Should the poem include emojis ?
-#' @param chat A Chat object, e.g. [ellmer::chat_openai()]. The default uses
+#' @param chat A [ellmer::Chat] object, e.g. [ellmer::chat_openai()]. The default uses
 #'   the 'gpt-3.5-turbo' model from OpenAI.
+#' @inheritParams rlang::args_error_context
 #'
 #' @return A lovely poem for your package crush
 #'
-#' @importFrom glue glue
 #' @examples
 #' prompt("dplyr")
 #'
@@ -21,11 +21,31 @@
 #'   roses("dplyr")
 #' }
 #'
+#' @importFrom glue glue
 #' @importFrom ellmer chat_openai
+#' @importFrom cli cli_abort
+#' @importFrom rlang current_env
 #' @export
-roses <- function(pkg, hint = "", emoji = TRUE, chat = chat_openai(model = "gpt-3.5-turbo")) {
+roses <- function(pkg, hint = "", emoji = TRUE, chat = chat_openai(model = "gpt-3.5-turbo"), error_call = current_env()) {
+  error_handler <- function(e) {
+    cli_abort(c(
+      "Problem creating or communicating with the LLM.",
+      i = "See {.fn ellmer::chat_openai} and friends for details on how to create a {.cls Chat} object.}"
+    ), parent = e, call = error_call)
+  }
+
   prompt <- prompt(pkg, hint = hint, emoji = emoji)
-  chat$chat(prompt, echo = "text")
+
+  chat <- withCallingHandlers(force(chat), error = error_handler)
+
+  if (!inherits(chat, "Chat")) {
+    cli_abort(
+      c("The {.arg chat} argument must be a {.cls Chat} object, not {.obj_type_friendly {chat}}."),
+      call = error_call
+    )
+  }
+
+  withCallingHandlers(chat$chat(prompt, echo = "text"), error = error_handler)
 }
 
 #' @rdname roses
